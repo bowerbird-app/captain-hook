8d07ce92edc0a467fb10eb7b100fdb497eb509d2
Lim Choon Yun (Marco) <marco.cylim@gmail.com>
2026-03-19 06:42:05 +0000

    Install flat_pack v0.1.6 from GitHub
    - Pin flat_pack gem to tag v0.1.6 in Gemfile
    - Update Gemfile.lock to v0.1.6 (commit f2116d1)
    - Run flat_pack:install generator: add CSS import, configure importmap and Stimulus
    - Update @source/@import paths in CSS files to v0.1.6 gem directory
    - Rebuild Tailwind CSS assets

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
+
+# Pin FlatPack controllers
+pin_all_from FlatPack::Engine.root.join("app/javascript/flat_pack/controllers"), under: "controllers/flat_pack", to: "flat_pack/controllers"
